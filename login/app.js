fb209682af047f08e18fae1562aecd10ebd61f6b
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-08-16 20:11:28 +0500

    All pages UI Complete

---
+++ login/app.js
@@ -0,0 +1,6 @@
+let login = () => {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    console.log(email.value)
+    console.log(password.value)
+}
