19247c79a6994993ca2e82ed7b646b29f167a7d8
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-08-23 23:16:15 +0500

    Done Code

--- login/app.js
+++ login/app.js
@@ -1,13 +1,26 @@
 let login = () => {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
-    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-        .then((userCredential) => {
-            var user = userCredential.user;
-            console.log()
-        })
-        .catch((error) => {
-            var errorMessage = error.message;
-            console.log(errorMessage)
-        });
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    // .then((user)=>{
+    //     console.log(user)
+        
+        location.replace("../profile-page/profile-page.html")
+        // location.replace("../proile-page/profile-page.html")
+        // firebase.database().ref(`User-Information`).on("child_added", (data) => {
+            // console.log(data.val())
+    // })
+    
+
+
+        // .then((userCredential) => {
+            // var user = userCredential.user;
+        // })
+        // .catch((error) => {
+        //     var errorMessage = error.message;
+        //     console.log(errorMessage)
+        // })
 }
+    // ) }
+
+    
--- signup/app.js
+++ signup/app.js
@@ -3,16 +3,28 @@ let signup = () => {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
     let number = document.getElementById("number");
+    var user_key = firebase.database().ref("User-Information").push().key
     var user_data = {
         user_Name: username.value,
         email: email.value,
         password: password.value,
-        contact_Number: number.value
+        contact_Number: number.value,
+        key: user_key
     }
-    firebase.database().ref("Information").push(user_data)
-    swal("Good job!\n You login success ", "You clicked the button!", "success")
-    setTimeout(() => {
-        location.replace("../profile-page/profile-page.html")
+    console.log(user_data)
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
 
-    }, 4000);
+            firebase.database().ref("User-Information").child(user_key).set(user_data)
+            swal("Good job!\n You login success ", "You clicked the button!", "success")
+            setTimeout(() => {
+                location.replace("../profile-page/profile-page.html")
+
+            }, 3000);
+            console.log(user)
+        // }
+        // )
+        .catch((error) => {
+            var errorMessage = error.message;
+            console.log(errorMessage)
+        })
 }
